fb66c4477588c554293dd66e74df414ec1919d82
Leonardo Pinto Silva Calazans <leonardopintosilva@hotmail.com>
2021-02-17 23:13:06 -0300

    exercicio diretivas concluido

--- src/app/exercicio-diretivas/exercicio-diretivas.component.ts
+++ src/app/exercicio-diretivas/exercicio-diretivas.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 
+import { MEMES_AGRUPADOS_POR_CATEGORIA } from './exercicio-diretivas.constants';
+
 @Component({
   selector: 'app-exercicio-diretivas',
   templateUrl: './exercicio-diretivas.component.html',
   styleUrls: ['./exercicio-diretivas.component.scss']
 })
-export class ExercicioDiretivasComponent{
+export class ExercicioDiretivasComponent {
 
   onDisplay = false;
 
-  exchangeValue(){
+  exchangeValue() {
     this.onDisplay = !this.onDisplay; // ele exibe ele mesmo negado
   }
-  onSum(number1: any, number2: any){
+  onSum(number1: any, number2: any) {
     return number1 + number2;
   }
 
@@ -28,18 +30,20 @@ export class ExercicioDiretivasComponent{
     placa: 'JND-5486',
     color: 'preto',
     model: 'Fusca',
-  },{
+  }, {
     placa: 'AAA-5486',
     color: 'azul',
     model: 'Logan',
-  },{
+  }, {
     placa: 'FFF-5486',
     color: 'branco',
     model: 'Esporte',
-  },{
+  }, {
     placa: 'EEE-5486',
     color: 'amarelo',
     model: 'Taxi',
   },];
 
+  memes = MEMES_AGRUPADOS_POR_CATEGORIA;
+  PREFIXO_IMAGEM_URL = 'https://raw.githubusercontent.com/vitorfgsantos/curso-angular-diretivas/master/images';
 }
